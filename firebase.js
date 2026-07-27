3027c937f34a3f5e2f895712d4e1bbf119f70f99
thanhgianghuynh009-dotcom <thanhgianghuynh009@gmail.com>
2026-07-27 11:21:00 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAnhuCn6qsiHYsQQwDAg5IP9H9OXYTT6Ts",
@@ -8,10 +7,10 @@ const firebaseConfig = {
   projectId: "binshop-8006f",
   storageBucket: "binshop-8006f.firebasestorage.app",
   messagingSenderId: "1093646474699",
-  appId: "1:1093646474699:web:4e07a9e3fc28a543562a4e"
+  appId: "1:1093646474699:web:4e07a9e3fc28a543562a4e",
+  measurementId: "G-XXEZWHVM4N"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
