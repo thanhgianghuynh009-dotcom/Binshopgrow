3b31b92b2b0255d7975848ab770e354ca255f9be
thanhgianghuynh009-dotcom <thanhgianghuynh009@gmail.com>
2026-07-27 11:22:47 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,79 @@
+import { auth } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+// ===== ĐĂNG KÝ =====
+
+const registerBtn = document.getElementById("registerBtn");
+
+if (registerBtn) {
+
+  registerBtn.onclick = () => {
+
+    let email = document.getElementById("registerEmail").value;
+    let password = document.getElementById("registerPassword").value;
+    let password2 = document.getElementById("registerPassword2").value;
+
+
+    if (password !== password2) {
+      alert("Mật khẩu không trùng!");
+      return;
+    }
+
+
+    createUserWithEmailAndPassword(auth, email, password)
+
+    .then(() => {
+
+      alert("Đăng ký thành công!");
+      window.location.href = "login.html";
+
+    })
+
+    .catch((error) => {
+
+      alert(error.message);
+
+    });
+
+  };
+
+}
+
+
+
+// ===== ĐĂNG NHẬP =====
+
+const loginBtn = document.getElementById("loginBtn");
+
+
+if (loginBtn) {
+
+  loginBtn.onclick = () => {
+
+    let email = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginPassword").value;
+
+
+    signInWithEmailAndPassword(auth, email, password)
+
+    .then(() => {
+
+      alert("Đăng nhập thành công!");
+      window.location.href = "index.html";
+
+    })
+
+    .catch((error) => {
+
+      alert(error.message);
+
+    });
+
+  };
+
+}
